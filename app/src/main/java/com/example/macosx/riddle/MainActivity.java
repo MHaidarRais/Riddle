154e49c7534fea1e1026b75fa33c5008d94a5a6f
HaidarRais <zwingfame@gmail.com>
2018-03-05 15:00:36 +0700

    updet 2
    updet 2

--- app/src/main/java/com/example/macosx/riddle/MainActivity.java
+++ app/src/main/java/com/example/macosx/riddle/MainActivity.java
@@ -13,15 +13,21 @@ public class MainActivity extends AppCompatActivity {
 
     String[] namaBuah= {"Kemampuan Anak ku","Makan Malam",
                         "Perampokan","Selangkah Menjadi Dokter",
-                        "Berburu Hantu","Cerita Website","Liburan Panjang"};
+                        "Berburu Hantu","Cerita Website","Liburan Panjang","Bebas Penjara",
+                        "Kisah Sang Tentara","Email Terakhir","Perampokan Bank"};
     int[] gambarBuah = {R.drawable.child,R.drawable.dinner,R.drawable.robb,
-                        R.drawable.dokter,R.drawable.school,R.drawable.website,R.drawable.hotel};
+                        R.drawable.dokter,R.drawable.school,R.drawable.website,R.drawable.hotel,
+                        R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
+                        R.drawable.ic_launcher_background};
     int[] detail = {R.string.cerita_bocah,R.string.cerita_makanmalam,
                     R.string.cerita_perampokan,R.string.cerita_Dokter,
-                    R.string.cerita_berburu,R.string.cerita_website,R.string.cerita_liburan};
+                    R.string.cerita_berburu,R.string.cerita_website,R.string.cerita_liburan,R.string.cerita_penjara,
+                    R.string.cerita_tentara,R.string.cerita_email_terakhir,R.string.cerita_perampokanbank};
     int[] jawaban = {R.string.jawaban_cerita_bocah,R.string.jawaban_cerita_makanmalam,
                     R.string.jawaban_cerita_perampokan,R.string.jawaban_cerita_dokter,
-                    R.string.jawaban_cerita_berburu,R.string.jawaban_cerita_website,R.string.jawaban_cerita_liburan};
+                    R.string.jawaban_cerita_berburu,R.string.jawaban_cerita_website,R.string.jawaban_cerita_liburan,
+                    R.string.jawaban_cerita_penjara,R.string.jawaban_cerita_tentara,R.string.jawaban_cerita_email_terakhir,
+                    R.string.jawaban_cerita_perampokanbank};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
